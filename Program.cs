8312101d73eeb4080628d2ce2f1ee4ade294bf8c
Fabricio <elfacio7219@gmail.com>
2026-02-25 12:27:14 -0500

    Primer Avance

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+public class Clientes
+{
+    public int id {get; set;}
+    public string? cedula {get; set;}
+    public string? nombre {get; set;}
+    public string? correo {get; set;}
+    public DateTime fecha {get; set;}
+    public Boolean activo {get; set;}
+    
+}
+
+public class Productos
+{
+    
+}
+
+public class Ventas
+{
+    
+}
+public class Venta_productos
+{
+    
+}
